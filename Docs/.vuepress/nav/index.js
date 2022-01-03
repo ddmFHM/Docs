34e4681c1abbb5d84347f947024d53653eb897d0
ddmFHM <ddmFHM@163.com>
2022-01-03 22:04:37 +0800

    init Docs

---
+++ Docs/.vuepress/nav/index.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    text: '前端',
+    items: [{
+        text: '基础',
+        items: [{
+            text: 'HTML',
+            link: '/1.前端/HTML/1.Doctype.md'
+          },
+          {
+            text: 'CSS',
+            link: '/1.前端/CSS/'
+          },
+          {
+            text: 'JS',
+            link: '/1.前端/JS/1.数据类型.md'
+          },
+          {
+            text: 'TS',
+            link: '/1.前端/TS/'
+          }
+        ]
+      }],
+  },
+
+  {
+    text: '个人网站',
+    link: 'https://www.ddmfhm.com'
+  },
+]
